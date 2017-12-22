4b77f6243b6cdab1e29323b54d79d37fd10be639
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-22 05:40:42 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,14 @@
 def turn_count(board)
   count = 0
   for square in board
-    if ["X", "O"].include?(square)
+    if ["X", "O"].include? square
       count += 1
     end
   end
   count
 end
+
+
 
 board = [" ", " ", "O", " ", "X", " ", "X", " ", " "]
 puts turn_count(board)
