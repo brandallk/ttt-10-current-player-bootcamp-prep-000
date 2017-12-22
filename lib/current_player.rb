a088dd40acb6cf28e9ecd20b099e9d04f249b246
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-22 05:44:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,6 +12,6 @@ def current_player(board)
   (turn_count(board) % 2 == 0) ? "X" : "O"
 end
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", "O", " ", "X", " ", " ", " ", " "]
 puts turn_count(board)
 puts current_player(board)
